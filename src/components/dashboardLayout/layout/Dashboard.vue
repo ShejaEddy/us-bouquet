c7d9bd8a3dfb70433d4aa7d440f9f387ee3fde2e
iradukunda1 <iradujdd4@gmail.com>
2020-02-05 08:03:08 +0800

    ~ made some changes.

---
+++ src/components/dashboardLayout/layout/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="wrapper">
+        <navbar/>
+        <sidebar/>
+        <content-view/>
+    </div>
+</template>
+<script>
+    import navbar from './navbar';
+    import sidebar from "./sidebar";
+    import contentView from "./content";
+
+    export default {
+        components: {
+            navbar,
+            sidebar,
+            contentView
+        }
+    }
+</script>
+<style>
+    .dropdown-toggle::after {
+        display: none !important;
+    }
+</style>
